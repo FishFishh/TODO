3da5cec88884f8fbc6444077bba62a2e6847821b
fish214057 <2140579318@qq.com>
2022-03-03 21:25:00 +0800

    second

--- src/main/java/com/todo/demo/DemoApplication.java
+++ src/main/java/com/todo/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.todo.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
-@MapperScan("")
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@MapperScan("com.todo.demo.Mapper")
 public class DemoApplication {
 
     public static void main(String[] args) {
