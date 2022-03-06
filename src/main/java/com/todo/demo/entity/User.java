4c45fcdce952c8c29ab8c6b8b80027990e82ca60
fish214057 <2140579318@qq.com>
2022-03-06 14:20:06 +0800

    Changes

--- src/main/java/com/todo/demo/entity/User.java
+++ src/main/java/com/todo/demo/entity/User.java
@@ -1,10 +1,14 @@
 package com.todo.demo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data//自动生成getter和setter
+@NoArgsConstructor//无参构造
+@AllArgsConstructor//有参构造
 public class User {
-    private Integer userId;
-    private String userName;
-    private String userPassword;
+    private Integer Id;
+    private String Name;
+    private String Password;
 }
