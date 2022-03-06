7f0e8be198d72001a3acb608e777409ef6c3339e
fish214057 <2140579318@qq.com>
2022-03-06 20:03:10 +0800

    Changes

--- src/main/java/com/todo/demo/service/UserService.java
+++ src/main/java/com/todo/demo/service/UserService.java
@@ -1,4 +1,13 @@
 package com.todo.demo.service;
 
+import com.todo.demo.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+    List<User> findAll();
+    User findById();
+    User insert(String name,String password);
+    Integer deleteById(int id);
+    Integer findId(int id);
 }
---
+++ src/main/java/com/todo/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.todo.demo.service.impl;
+
+import com.todo.demo.entity.User;
+import com.todo.demo.mapper.UserMapper;
+import com.todo.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public List<User> findAll() {
+        return userMapper.findAll();
+    }
+
+    @Override
+    public User findById() {
+        return null;
+    }
+
+    @Override
+    public User insert(String name, String password) {
+        return null;
+    }
+
+    @Override
+    public Integer deleteById(int id) {
+        return null;
+    }
+
+    @Override
+    public Integer findId(int id) {
+        return null;
+    }
+}
