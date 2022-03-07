cbd6f4f4a506ba16b6624f9b6f2336f2db582fbd
fish214057 <2140579318@qq.com>
2022-03-07 20:11:38 +0800

    Changes

---
+++ src/main/java/com/todo/demo/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.todo.demo.mapper;
+
+import com.todo.demo.entity.User;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Mapper//mapper类 DAO层
+@Repository
+public interface UserMapper {
+    User login(String name,String password);
+    List<User> findAll();
+    User findById();
+    User insert(String name,String password);
+    Integer deleteById(int id);
+    Integer findId(int id);
+}
